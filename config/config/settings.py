14be7699424f0ceeff4942aa40cf2b28ca0103d4
mamataliewaaiko@gmail.com <mamataliewaaiko@gmail.com>
2026-03-21 22:21:34 +0600

    feat: project setup

--- config/config/settings.py
+++ config/config/settings.py
@@ -1,10 +1,7 @@
-
-
 from pathlib import Path
 from datetime import timedelta
-from dotenv import load_dotenv
-from django.conf.global_settings import MEDIA_URL, MEDIA_ROOT, AUTH_USER_MODEL
 import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -137,12 +134,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
--- config/config/urls.py
+++ config/config/urls.py
@@ -1,19 +1,3 @@
-"""
-URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.core.cache import cache
 from django.urls import path, include
@@ -37,7 +21,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
 #     path('api/auth/', include('apps.users.urls')),
-#     path('api/submissions/', include('appa.submissions.urls')),
+#     path('api/submissions/', include('apps.submissions.urls')),
 
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='swagger_ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='redoc'),
